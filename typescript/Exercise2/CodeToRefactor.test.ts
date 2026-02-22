cbfee9609f6ca67d2a1cc90320df4893e06c1415
MinJustDoIt <minthihalwin.dev@gmail.com>
2026-02-22 19:08:17 +1300

    fix: update import statement to use Review namespace for People constructor

--- typescript/Exercise2/CodeToRefactor.test.ts
+++ typescript/Exercise2/CodeToRefactor.test.ts
@@ -1,8 +1,8 @@
-import { People } from "./CodeToRefactor";
+import { Review } from "./CodeToRefactor";
 
 describe("People - constructor", () => {
-    it("should create a person with only a name", () => {
-        const person = new People('Alice');
-        expect(person.Name).toBe('Alice');
-    })
+  it("should create a person with only a name", () => {
+    const person = new Review.People("Alice");
+    expect(person.Name).toBe("Alice");
+  });
 });
